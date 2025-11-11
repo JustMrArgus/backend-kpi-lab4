6313777ed85e417ef0465ca10f16b522e7717204
JustMrArgus <argusprogramming@protonmail.com>
2025-11-11 02:18:42 +0200

    Add token creating function

---
+++ utils/createAndSendToken.js
@@ -0,0 +1,24 @@
+const signToken = require("./signToken");
+
+module.exports = (user, statusCode, res) => {
+  const token = signToken(user.id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  cookieOptions.secure = true;
+
+  res.cookie("jwt", token, cookieOptions);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
